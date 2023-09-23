efcdb40b82eacce698ddd3406495967d7931b8b6
myfieewifi <347691882@qq.com>
2023-09-23 13:35:58 +0800

    tvbox点播转直播源更新

--- js/直播.js
+++ js/直播.js
@@ -1,10 +1,9 @@
 [
-    {"name": "MV合集",     "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "MTV合集",     "url": "https://qu.ax/KmzB.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "乱世",     "url": "http://www.dmtv.ml/mao/live/m3u.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "吾爱",     "url": "http://52bsj.vip:81/api/v3/file/get/763/live.txt?sign=87BTGT1_6AOry7FPwy_uuxFTv2Wcb9aDMj46rDdRTD8%3D%3A0&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "饭太硬",     "url": "http://ftyyy.tk/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"}
+    {"name": "MV合集","url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "MTV合集","url": "https://qu.ax/KmzB.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "甜蜜","url": "http://zdir.kebedd69.repl.co/public/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "俊于","url": "http://home.jundie.top:81/Cat/tv/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "菜妮丝","url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "月光宝盒","url": "http://52bsj.vip:81/api/v3/file/get/79119/ZB.txt?sign=iChDXDC7WJRTp7yWAyVdbY3si5sJ5eFwEgaR35YQGSo%3D%3A0&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "Yoursmile7","url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
 ]
