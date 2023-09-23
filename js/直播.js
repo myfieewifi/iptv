5240e4f93a6396a8dc110f85d11f306da8e836e8
myfieewifi <347691882@qq.com>
2023-09-23 20:33:26 +0800

    tvbox点播转直播源更新

--- js/直播.js
+++ js/直播.js
@@ -4,6 +4,7 @@
     {"name": "甜蜜",     "url": "http://zdir.kebedd69.repl.co/public/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
     {"name": "俊于",     "url": "http://home.jundie.top:81/Cat/tv/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
     {"name": "菜妮丝",     "url": "http://xn--ihqu10cn4c.xn--z7x900a.love:63/TV/tvzb.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "饭太硬",     "url": "https://agit.ai/fantaiying/0/raw/branch/main/tvlive.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
     {"name": "月光宝盒",     "url": "http://52bsj.vip:81/api/v3/file/get/79119/ZB.txt?sign=iChDXDC7WJRTp7yWAyVdbY3si5sJ5eFwEgaR35YQGSo%3D%3A0&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
     {"name": "Yoursmile7",     "url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"}
 ]
