63f7f880890012d43fc4e07012e03505b8d3327f
myfieewifi <347691882@qq.com>
2023-10-09 16:11:50 +0800

    点播源更新

--- js/厂长资源.js
+++ js/厂长资源.js
@@ -1,5 +1,6 @@
 var rule={
     title:'厂长资源',
+    // host:'https://czspp.com',
     host:'https://www.czzy.site',
     hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});HOST = jsp.pdfh(html,"h3&&a&&href")',
     url:'/fyclassfyfilter',
