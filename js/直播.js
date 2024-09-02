04410c1381d4071dfb8b4a1e50a03fb3eef88030
myfieewifi <347691882@qq.com>
2024-09-02 11:24:23 +0800

    代理地址更新

--- js/直播.js
+++ js/直播.js
@@ -1,5 +1,5 @@
 [
-    {"name": "MV合集", "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "MTV合集", "url": "https://qu.ax/KmzB.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
-    {"name": "Yoursmile7", "url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/live.txt&&&http://ghproxy.net/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"}
+    {"name": "MV合集", "url": "https://agit.ai/guot54/ygbh/raw/branch/master/zB/mv.txt&&&http://mirror.ghproxy.com/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "MTV合集", "url": "https://qu.ax/KmzB.txt&&&http://mirror.ghproxy.com/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"},
+    {"name": "Yoursmile7", "url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/live.txt&&&http://mirror.ghproxy.com/raw.githubusercontent.com/myfieewifi/iptv/main/images/zbt.jpg"}
 ]
